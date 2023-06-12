7ba4790e95278554a6104f07964d5c5193ff14c1
Matthieu <matthieu.perrin@montreal.ca>
2023-06-12 14:02:58 -0400

    feature(performance): test against gofs lite instead of inquiry
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>

--- le-taxi-api-tests/src/taxis/loadTests/generatePayload.ts
+++ le-taxi-api-tests/src/taxis/loadTests/generatePayload.ts
@@ -1,39 +1,31 @@
 // Licensed under the AGPL-3.0 license.
 // See LICENSE file in the project root for full license information.
 import { generateSouthShoreLat, generateSouthShoreLon } from '../../shared/commonLoadTests/specialRegion';
-import { IInquirySharedState } from './IInquirySharedState';
+import { IMotorSharedState } from './IMotorSharedState';
 
 // tslint:disable-next-line: no-console
 console.log('WARNING: ALL LOAD TESTS MUST BE EXECUTED WITH A SINGLE REPLICA PER DEPLOYMENT!');
 
-const sharedStateJson = require('fs').readFileSync('src/taxis/loadTests/inquiry.sharedState.json');
+const sharedStateJson = require('fs').readFileSync('src/taxis/loadTests/motor.sharedState.json');
 
-export const sharedState: IInquirySharedState = JSON.parse(sharedStateJson);
+export const sharedState: IMotorSharedState = JSON.parse(sharedStateJson);
 
-export function generateStandardPayloadInquiry(context: any, ee: any, next: any) {
-  return generatePayloadInquiry(context, ee, next, 'taxi-registry-standard');
+export function generateStandardPayloadWaitTime(context: any, ee: any, next: any) {
+  return generatePayloadWaitTime(context, ee, next, 'taxi-registry-standard');
 }
 
-export function generateSpecialNeedPayloadInquiry(context: any, ee: any, next: any) {
-  return generatePayloadInquiry(context, ee, next, 'taxi-registry-special-need');
+export function generateSpecialNeedPayloadWaiTime(context: any, ee: any, next: any) {
+  return generatePayloadWaitTime(context, ee, next, 'taxi-registry-special-need');
 }
 
-function generatePayloadInquiry(context: any, ee: any, next: any, assetType: string) {
+function generatePayloadWaitTime(context: any, ee: any, next: any, assetType: string) {
   context.vars.apikey = sharedState.searchMotor.apiKey;
   context.vars.body = {
-    from: {
-      coordinates: {
-        lat: generateSouthShoreLat(),
-        lon: generateSouthShoreLon()
-      }
-    },
-    to: {
-      coordinates: {
-        lat: generateSouthShoreLat(),
-        lon: generateSouthShoreLon()
-      }
-    },
-    useAssetTypes: [assetType]
+    pickup_lat: generateSouthShoreLat(),
+    pickup_lon: generateSouthShoreLon(),
+    drop_off_lat: generateSouthShoreLat(),
+    drop_off_lon: generateSouthShoreLon(),
+    brand_id: [assetType]
   };
   return next();
 }
