e4743e0c0f164a4be0509c6ee2e80a47e1baf54e
Matthieu <matthieu.perrin@montreal.ca>
2023-11-15 12:03:28 -0500

    fix: fix no brandId found in query params for realtime_booking endpoint

--- le-taxi-api-node.js/src/features/gofsLite/gofsLite.validators.ts
+++ le-taxi-api-node.js/src/features/gofsLite/gofsLite.validators.ts
@@ -10,7 +10,7 @@ import { gofsLiteMapper } from './gofsLite.mapper';
 
 export async function validateGofsLiteRealtimeBookingRequest(request: Request): Promise<InquiryRequest> {
   const brandId = request.query.brand_id as string;
-  const brandIdArray = brandId.length > 0 ? brandId.split(',') : [];
+  const brandIdArray = brandId?.length > 0 ? brandId.split(',') : [];
   const requestDto = { ...request.query, brand_id: brandIdArray}
   await validateDtoProperties(new GofsLiteRealtimeBookingRequestDto(), requestDto);
   return validateInquiryRequest(gofsLiteMapper.toInquiryRequest(requestDto as GofsLiteRealtimeBookingRequestDto));
