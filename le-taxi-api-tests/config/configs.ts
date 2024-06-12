e09f055696380f309d0604c6240d39d6365a7c28
Matthieu <matthieu.perrin@montreal.ca>
2024-06-12 10:09:31 -0400

    feat: make taxi expiration in load tests optional
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>

--- le-taxi-api-tests/config/configs.ts
+++ le-taxi-api-tests/config/configs.ts
@@ -1,9 +1,9 @@
 // Licensed under the AGPL-3.0 license.
 // See LICENSE file in the project root for full license information.
-import * as config from 'config';
-import * as path from 'path';
-import { ConfigCache } from './configCache';
-import { constants } from './constants';
+import * as config from "config";
+import * as path from "path";
+import { ConfigCache } from "./configCache";
+import { constants } from "./constants";
 
 /**
  * Configurations for the application.
@@ -30,7 +30,9 @@ export class Configs {
   private constructor() {
     this.root = path.normalize(`${__dirname}/..`);
     this.theEnvironment = config.util.getEnv(constants.EnvVariables.NODE_ENV);
-    this.theEnvironmentInstance = config.util.getEnv(constants.EnvVariables.NODE_APP_INSTANCE);
+    this.theEnvironmentInstance = config.util.getEnv(
+      constants.EnvVariables.NODE_APP_INSTANCE
+    );
     this.cache = new ConfigCache();
   }
 
@@ -54,12 +56,13 @@ export class Configs {
         : this.theEnvironment,
       type: this.theEnvironment,
       instance: this.theEnvironmentInstance,
-      isLocal: this.cache.get<boolean>('environment.isLocal'),
+      isLocal: this.cache.get<boolean>("environment.isLocal"),
       isDev: this.theEnvironment === constants.Environments.DEV,
       isLocalOrDev:
-        this.cache.get<boolean>('environment.isLocal') || this.theEnvironment === constants.Environments.DEV,
+        this.cache.get<boolean>("environment.isLocal") ||
+        this.theEnvironment === constants.Environments.DEV,
       isAcc: this.theEnvironment === constants.Environments.ACC,
-      isProd: this.theEnvironment === constants.Environments.PROD
+      isProd: this.theEnvironment === constants.Environments.PROD,
     };
   }
 
@@ -68,7 +71,22 @@ export class Configs {
    */
   get e2eTesting() {
     return {
-      maxTestConcurrency: this.cache.get<number>('e2eTesting.maxTestConcurrency')
+      maxTestConcurrency: this.cache.get<number>(
+        "e2eTesting.maxTestConcurrency"
+      ),
+    };
+  }
+
+  /**
+   * Load testing informations
+   */
+  get loadTesting() {
+    return {
+      snapshots: {
+        runWithTaxiExpiration: this.cache.get<number>(
+          "loadTesting.snapshots.runWithTaxiExpiration"
+        ),
+      },
     };
   }
 
@@ -77,9 +95,9 @@ export class Configs {
    */
   get apiTests() {
     return {
-      rootApiKey: this.cache.get<string>('apiTests.rootApiKey'),
-      user: this.cache.get<string>('apiTests.user'),
-      password: this.cache.get<string>('apiTests.password')
+      rootApiKey: this.cache.get<string>("apiTests.rootApiKey"),
+      user: this.cache.get<string>("apiTests.user"),
+      password: this.cache.get<string>("apiTests.password"),
     };
   }
 
@@ -88,9 +106,15 @@ export class Configs {
    */
   get inquiries() {
     return {
-      delayToExceedPromotion: this.cache.get<number>('inquiries.delayToExceedPromotion'),
-      fixedDailyPriceDowntownToAirport: this.cache.get<number>('inquiries.fixedDailyPriceDowntownToAirport'),
-      fixedNightlyPriceDowntownToAirport: this.cache.get<number>('inquiries.fixedNightlyPriceDowntownToAirport')
+      delayToExceedPromotion: this.cache.get<number>(
+        "inquiries.delayToExceedPromotion"
+      ),
+      fixedDailyPriceDowntownToAirport: this.cache.get<number>(
+        "inquiries.fixedDailyPriceDowntownToAirport"
+      ),
+      fixedNightlyPriceDowntownToAirport: this.cache.get<number>(
+        "inquiries.fixedNightlyPriceDowntownToAirport"
+      ),
     };
   }
 
@@ -99,7 +123,9 @@ export class Configs {
    */
   get caching() {
     return {
-      delayToExceedUsersCache: this.cache.get<number>('caching.delayToExceedUsersCache')
+      delayToExceedUsersCache: this.cache.get<number>(
+        "caching.delayToExceedUsersCache"
+      ),
     };
   }
 
@@ -108,12 +134,16 @@ export class Configs {
    */
   get taxiRegistryOsrmApi() {
     return {
-      base: this.cache.get<string>('taxiRegistryOsrmApi.base'),
-      domainPath: this.cache.get<string>('taxiRegistryOsrmApi.domainPath'),
+      base: this.cache.get<string>("taxiRegistryOsrmApi.base"),
+      domainPath: this.cache.get<string>("taxiRegistryOsrmApi.domainPath"),
       estimation: {
-        durationBias: this.cache.get<number>('taxiRegistryOsrmApi.estimation.durationBias'),
-        requestAndDispatchInSec: this.cache.get<number>('taxiRegistryOsrmApi.estimation.requestAndDispatchInSec')
-      }
+        durationBias: this.cache.get<number>(
+          "taxiRegistryOsrmApi.estimation.durationBias"
+        ),
+        requestAndDispatchInSec: this.cache.get<number>(
+          "taxiRegistryOsrmApi.estimation.requestAndDispatchInSec"
+        ),
+      },
     };
   }
 }
