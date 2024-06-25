7e1995dd26d7354e8e8be82cd3245172882287a3
Matthieu <matthieu.perrin@montreal.ca>
2024-06-25 08:37:15 -0400

    feat: rework osrm tests
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>
    
    feat: add missing license
    
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>
    
    feat: increase performance test to 2 hours
    
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>

---
+++ le-taxi-api-tests/src/osrm/loadTests/generateOsrmPayload.ts
@@ -0,0 +1,26 @@
+// Licensed under the AGPL-3.0 license.
+// See LICENSE file in the project root for full license information.
+import { getRandomNumberBetween } from "../../shared/commonLoadTests/randomData";
+import { generatePointForLoadTest } from "../../shared/commonLoadTests/specialRegion";
+
+// tslint:disable-next-line: no-console
+console.log(
+  "WARNING: ALL LOAD TESTS MUST BE EXECUTED WITH A SINGLE REPLICA PER DEPLOYMENT!"
+);
+
+export function beforeGetRoute(context: any, ee: any, next: any) {
+  const source = generatePointForLoadTest();
+  const dest = generatePointForLoadTest();
+  context.vars.taxiLat = source.lat;
+  context.vars.taxiLon = source.lon;
+  context.vars.clientLat = addOffset(source.lat);
+  context.vars.clientLon = addOffset(source.lon);
+  context.vars.destinationLat = dest.lat;
+  context.vars.destinationLon = dest.lon;
+  return next();
+}
+
+function addOffset(value: number) {
+  const offset = getRandomNumberBetween(-0.015, 0.015);
+  return value + offset;
+}
