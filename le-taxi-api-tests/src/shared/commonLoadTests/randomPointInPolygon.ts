cb2ee0ad1d9acd2c02c0d957297022b267806dc7
Matthieu <matthieu.perrin@montreal.ca>
2024-06-25 08:41:39 -0400

    feat: add missing license
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>

--- le-taxi-api-tests/src/shared/commonLoadTests/randomPointInPolygon.ts
+++ le-taxi-api-tests/src/shared/commonLoadTests/randomPointInPolygon.ts
@@ -1,3 +1,6 @@
+// Licensed under the AGPL-3.0 license.
+// See LICENSE file in the project root for full license information.
+// Original Code from https://github.com/apburnes/random-points-on-polygon
 "use strict";
 
 var extent = require("turf-extent");
